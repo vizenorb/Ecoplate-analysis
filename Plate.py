04f58eff4602c286970ad191deb5752ea6671635
Brady Vizenor <vizenorb@gmail.com>
2017-10-10 14:34:30 -0500

    added __iter__ and __getitem__ functions to plate class

--- Plate.py
+++ Plate.py
@@ -7,10 +7,10 @@ Description:
 Preconditions:  
 Postconditions: 
 '''
-
-#create plate class 
-class Plate:
-    sourceMatrix = []
+#Plate class inherits from the abstract class 'Sequence'
+from collections.abc import Sequence
+class Plate(Sequence):
+    _sourceMatrix = []
 
     def __init__(self, rawdata):
         '''
@@ -18,16 +18,17 @@ class Plate:
         Preconditions:  "rawdata" is a string containing an ecoplate scan output
         Postconditions: None
         '''
-        dataMatrix = []
+        self._dataMatrix = []
         #splits the initial string input into a list where each item is a single line
         rawdataList = rawdata.split("\n")
         #selects out the data we want to use, which starts at the "529" wavelength header
-        dataList = rawdataList[rawdataList.index("529")+1:rawdataList.index("529")+9]
+        self._dataList = rawdataList[rawdataList.index("529")+1:rawdataList.index("529")+9]
         #splits each line into a list containing a single reading, puts them into another list \
         # to act as a 2D array, then removes the useless number at the end of each line
         for i in range(len(dataList)):
-            dataMatrix.append([dataList[i].split('\t')])
-            del dataMatrix[i][-1]
+            self._dataMatrix.append([self._dataList[i].split('\t')])
+            if self._dataMatrix[i][-1] == "529":     #just to be safe
+                del self._dataMatrix[i][-1]
 
     def getSources():
         '''
@@ -39,6 +40,28 @@ class Plate:
         colList.insert(0,"\t")
         rowList = [x+1 for x in range(len(sourceMatrix[0]))]
         colList.insert(0,"\t")
+        #Create a string that has a number of formatted values
+        for row in self._sourceMatrix:
+            #x is incremented to create a series of sequential numbers independent of the loop
+            for i in range(x,x+len(row)):
+                returnStr += "$"+str(i)+".30s"
+                x += 1
+            returnStr +="\n"
+        #removes all spaces and inserts a tab so everyhing looks clean
+        returnStr.replace(" ","\t")
+        #Create a list that will contain the values we will assign to each format variable
+        sourceFormatList = []
+        for row in sourceMatrix:
+            for entry in sourceMatrix:
+                sourceFormatList += entry       
+        return returnStr.format(sourceFormatList)
+
+
+    def __getitem__(self, key):
+        return self._dataMatrix[key]
+
+    def __iter__(self):
+        return(iter(self._dataMatrix))
 
     def __str__(self):
         '''
@@ -49,7 +72,7 @@ class Plate:
         returnStr = ''
         x = 0
         #Create a string that has a number of formatted values
-        for row in self.dataMatrix:
+        for row in self._dataMatrix:
             for i in range(x,x+len(row)):
                 returnStr += "$"+str(i)+".30s "
                 x += 1
