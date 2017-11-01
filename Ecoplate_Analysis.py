a963f3cf3d6e246c8d97541820fd3249e734f49e
Brady Vizenor <vizenorb@gmail.com>
2017-11-01 14:34:17 -0500

    added new workaround for missing data audit trails

--- Ecoplate_Analysis.py
+++ Ecoplate_Analysis.py
@@ -70,7 +70,7 @@ for fileName in fileList:
     #creates a date object using the date in the filename, with format:  datetime.datetime(YYYY,MM,DD)
     #calculate me from file itself instead, more reliable                       newdate = datetime.datetime(int(fileName[0:4]),int(fileName[4:6]),int(fileName[6:8]))
     #grabs the plate ID defined as any digit in the second half of the file name
-    plateID = ''.join([n for n in fileName.split()[1] if n.isdigit()])
+    plateID = ''.join([n for n in fileName.split()[-1] if n.isdigit()])
                 #newdate = datetime.datetime(int(fileName[0:4]),int(fileName[4:6]),int(fileName[6:8]))
     
     #opens the file in read mode
@@ -99,17 +99,22 @@ for fileName in fileList:
 
 
     # Next, get a date/time
-    dateList = rawdataList[rawdataList.index("Data Audit Trail"):]
-    for dLine in dateList:
-        if "Plate read successfully completed" in dLine:
-            dateLine = dLine
-    dateVals = dateLine.split()[0].split("/")
-    timeVals = dateLine.split()[1].split(":")
-    if "PM" in dateLine and " 12:" not in dateLine:
-        timeVals[0] += 12
-    if "AM" in dateLine and " 12:" in dateLine:
-        timeVals[0] = 0
-    newdate = datetime.datetime(int(dateVals[2]),int(dateVals[0]),int(dateVals[1]),int(timeVals[0]),int(timeVals[1]),int(timeVals[2]))
+    try:
+        dateList = rawdataList[rawdataList.index("Data Audit Trail"):]
+        for dLine in dateList:
+            if "Plate read successfully completed" in dLine:
+                dateLine = dLine
+        dateVals = dateLine.split()[0].split("/")
+        timeVals = dateLine.split()[1].split(":")
+        if "PM" in dateLine and " 12:" not in dateLine:
+            timeVals[0] += 12
+        if "AM" in dateLine and " 12:" in dateLine:
+            timeVals[0] = 0
+        newdate = datetime.datetime(int(dateVals[2]),int(dateVals[0]),int(dateVals[1]),int(timeVals[0]),int(timeVals[1]),int(timeVals[2]))
+    except ValueError:
+        print("file is missing data audit trail")
+        print("continuing with date in file name, but no time")
+        newdate = datetime.datetime(int(fileName[0:4]), int(fileName[4:6], int(fileName[6:8])))
     
     # Close the file.
     rd.close()
